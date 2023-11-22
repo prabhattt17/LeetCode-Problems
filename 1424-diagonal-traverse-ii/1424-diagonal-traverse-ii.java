3213a25fb2a5298b2c9c5506ef4b6cd6290dee59
prabhattt17 <87928913+prabhattt17@users.noreply.github.com>
2023-11-22 17:45:44 +0530

    Time: 30 ms (68.22%), Space: 77.5 MB (13.88%) - LeetHub

--- 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.java
+++ 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.java
@@ -3,15 +3,14 @@ class Solution {
      HashMap<Integer,List<Integer>> map = new HashMap<>();
         int row = 0 ,col = 0, te = 0;
         
-        for(int i = 0; i<nums.size(); i++){
-            for(int j = 0;j<nums.get(i).size(); j++){
+        for(int i = nums.size()-1; i>=0; i--){
+            for(int j = nums.get(i).size()-1;j>=0; j--){
                 row = Math.max(row,i);
                 col = Math.max(col,j);
                 
                 List<Integer> list = map.getOrDefault(i+j,new ArrayList<>());
                 
                 list.add(nums.get(i).get(j));
-                // System.out.println((i+j)+" "+nums.get(i).get(j));
                 map.put(i+j,list);
                 te++;
             }
@@ -22,7 +21,6 @@ class Solution {
         
         for(int i = 0;i<=row+col; i++){
             List<Integer> l = map.getOrDefault(i,new ArrayList<>());
-            Collections.reverse(l);
             
             for(int val:l){
                 ans[idx++] = val;
