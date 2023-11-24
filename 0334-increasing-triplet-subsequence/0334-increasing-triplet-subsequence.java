a5c8322669513e8076213c065d2a140466ab586a
prabhattt17 <87928913+prabhattt17@users.noreply.github.com>
2023-11-24 17:17:20 +0530

    Time: 2 ms (89.34%), Space: 132.5 MB (6.44%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
@@ -1,43 +1,17 @@
 class Solution {
     public boolean increasingTriplet(int[] nums) {
-        if(nums.length<3)return false;
         
-        int[] s=new int[nums.length];
-        int[] b=new int[nums.length];
-        
-        int min=0;
-        int max=0;//Integer.MIN_VALUE;
-        
-        s[0]=nums[0];
-        min=s[0];
-        for(int i=1;i<nums.length;i++){
-            if(nums[i]>min){
-                s[i]=min;
-                System.out.println(s[i]);
+        // 20,100,10,12,5,13
+        int one=Integer.MAX_VALUE,two=Integer.MAX_VALUE;
+        for(int i=0;i<=nums.length-1;i++){
+            if(one>=nums[i]){
+                one=nums[i];
+            }else if(two>=nums[i]){
+                two=nums[i];
             }else{
-                s[i]=nums[i];
-                min=nums[i];
+                return true;
             }
-        }    
-        
-        b[b.length-1]=nums[b.length-1];
-        max=b[b.length-1];
-        for(int i=nums.length-2;i>=0;i--){
-            if(nums[i]<max){
-                
-                b[i]=max;
-                System.out.println(s[i]);
-            }else{
-                b[i]=nums[i];
-                max=nums[i];
-            }
-        }   
-        
-        for(int i=1;i<nums.length-1;i++){
-
-        if(nums[i]>s[i] && nums[i]<b[i])return true;
         }
-        
         return false;
     }
 }
