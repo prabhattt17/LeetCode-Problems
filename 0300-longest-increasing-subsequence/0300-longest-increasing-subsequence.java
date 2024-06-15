289736139c426f36576753d1618f56b6264a0431
prabhattt17 <87928913+prabhattt17@users.noreply.github.com>
2024-06-15 22:30:32 +0530

    Time: 37 ms (73.10%), Space: 44 MB (56.83%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,19 +1,32 @@
 class Solution {
+    int max;
     public int lengthOfLIS(int[] nums) {
+        max = 1;
+        // helper(nums,Integer.MIN_VALUE,0,0);
         int[] dp = new int[nums.length];
-        int ans = 0;
         
-        for(int i = 0; i<nums.length; i++){
-            int max = 0;
-            for(int j = i-1; j>=0; j--){
+        dp[0] = 1;
+        Arrays.fill(dp,1);
+        for(int i = 1;i<nums.length; i++){
+            int count = 0;
+            for(int j = 0 ; j<i ; j++){
                 if(nums[i]>nums[j]){
-                    max = Math.max(max,dp[j]);
+                    dp[i] = Math.max(dp[i],dp[j]+1);
                 }
             }
-            dp[i] = max+1;
-            ans = Math.max(dp[i],ans);
+            max = Math.max(max, dp[i]);
         }
-        
-        return ans;
+        return max;
     }
+    
+//     public void helper(int[] nums, int prev, int idx, int count){
+//         if(idx>nums.length)return;
+        
+//         max = Math.max(count,max);
+//         if(idx<nums.length && nums[idx]>prev){
+//             helper(nums,nums[idx],idx+1,count+1);
+//         }
+//         dp[idx][]
+//         helper(nums,prev,idx+1,count);
+//     }
 }
