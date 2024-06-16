9f72b1bd27d0d53ab53a1bf72f751cef49daa8a3
prabhattt17 <87928913+prabhattt17@users.noreply.github.com>
2024-06-16 19:45:09 +0530

    Time: 13 ms (72.57%), Space: 45.4 MB (88.83%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,40 +1,33 @@
 class Solution {
-    public class Pair{
-        int elm;
+    class Pair{
+        int val;
         int frq;
-        Pair(int elm,int frq){
-            this.elm = elm;
+        
+        Pair(int val,int frq){
+            this.val = val;
             this.frq = frq;
         }
     }
+    
     public int[] topKFrequent(int[] nums, int k) {
         HashMap<Integer,Integer> map = new HashMap<>();
         
-        for(int i:nums){
-            map.put(i,map.getOrDefault(i,0)+1);
+        for(int n:nums){
+            map.put(n,map.getOrDefault(n,0)+1);
         }
         
-        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b)->a.frq-b.frq);
-        int[] ans = new int[k];
-        int idx = k-1;
-        
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b) -> b.frq-a.frq);
+
         for(int key:map.keySet()){
-            int frq = map.get(key);
-            
-            if(k==0 && pq.size()>0 && pq.peek().frq<frq){
-                pq.remove();
-                pq.add(new Pair(key,frq));
-            }else if(k>0){
-                pq.add(new Pair(key,frq));
-                k--;
-            }
+            pq.add(new Pair(key,map.get(key)));
         }
         
-    
+        int ans[] = new int[k];
+        int idx = 0;
         
-        while(pq.size()!=0){
+        while(k-->0){
             Pair p = pq.remove();
-            ans[idx--] = p.elm;
+            ans[idx++] = p.val;
         }
         
         return ans;
